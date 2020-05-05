9b97680763d5da216abbb49dd106e91dfad6b4ba
Duc Van <vanquangduc224@gmail.com>
2020-05-05 12:21:45 +0700

    Add 2 more funtions

--- general.py
+++ general.py
@@ -26,9 +26,22 @@ def append_to_file(path,data):
     with open(path, 'a') as file:
         file.write(data + '\n')
 
-#Delete the contents of a file
+# Delete the contents of a file
 def delete_file_contents(path):
     with open(path, 'w'):
         pass
 
-#hello
+# Read a file and convert each line to set items
+def file_to_set(file_name):
+    results = set()
+    with open(file_name,'rt') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+    return results
+
+# Iterate through a set, each item will be a new line in the file
+def set_to_file(links, file):
+    delete_file_contents(file)
+    for link in sorted(links):
+        append_to_file(file, link)
+
