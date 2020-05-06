2cb7a63ddb49a93c721312d6e0e9f49cf49c166c
Duc Van <vanquangduc224@gmail.com>
2020-05-06 14:37:07 +0700

    Add crawled_page function to spider.py

--- spider.py
+++ spider.py
@@ -28,3 +28,15 @@ class Spider:
         create_data_files(Spider.project_name, Spider.base_url) 
         Spider.queue = file_to_set(Spider.queue_file)
         Spider.crawled = file_to_set(Spider.crawled_file)
+    
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name + ' crawling '+ page_url)
+            print('Queue ' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()
+
+    
